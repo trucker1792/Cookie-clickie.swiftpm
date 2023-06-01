20610d8d2aa5cb7bf56c310f5821910b080eabb9
trucker1792 <ngajda5141@stu.d214.org>
2023-06-01 10:27:04 -0500

    mone

--- ContentView.swift
+++ ContentView.swift
@@ -1,64 +1,72 @@
 import SwiftUI
 
-    struct ContentView: View {
-        @State var counter = 0
-        @State var cpc = 1
-        @State var showAlert = false
-        @State var shouldHide = false
-        @State var MoonPoint = 0
-        
-        var body: some View {
-            VStack {
-                Image("cookie3")
-                Text("total clicks = \(counter)")
-                Text("click amount per click = \(cpc)")
-                Text("moon travel thingys = \(MoonPoint)")
-                    .font(.largeTitle)
-                    .foregroundColor(.green)
-                Text("Test")
-                HStack{
-                    Button {
-                        counter += cpc
-                    } label: {
-                        Text("click here to generate clicks")
-                    }
-                    
-                    Button(action: {counter += cpc}, label: {
-                        Text("click here to generate clicks")
-                            .foregroundColor(.blue)
-                    })
-                    
-                    if counter >= 50 {Button("upgrade", action: {counter -= 50; cpc += 1})
-                            .disabled(false)
-                    } else if counter <= 49 {
-                        Button("upgrade", action: {counter -= 0; cpc += 0})
-                            .disabled(true)
-                            .hidden()
-                    }
-                }
-                if counter == 69 {Button("funny", action: {counter -= 69; cpc += 419})
+struct ContentView: View {
+    @State var counter = 0
+    @State var cpc = 1
+    @State var showAlert = false
+    @State var shouldHide = false
+    @State var MoonPoint = 0
+    
+    var body: some View {
+        VStack {
+            Image("cookie3")
+            Text("total clicks = \(counter)")
+            Text("click amount per click = \(cpc)")
+            Text("moon travel thingys = \(MoonPoint)")
+                .font(.largeTitle)
+                .foregroundColor(.green)
+            
+                //                Button {
+                //                    counter += cpc
+                //                } label: {
+                //                    Text("click here to generate clicks")
+                //                }
+                
+                Button(action: {counter += cpc}, label: {
+                    Text("click here to generate clicks")
+                        .foregroundColor(.blue)
+                })
+                
+                if counter >= 50 {Button("upgrade (50)", action: {counter -= 50; cpc += 1})
                         .disabled(false)
-                } else if counter != 69 {Button("funny", action: {counter -= 69; cpc += 420})
+                } else if counter <= 49 {
+                    Button("upgrade (50)", action: {counter -= 0; cpc += 0})
                         .disabled(true)
-                        .hidden()
+                    
                 }
-                Image("moon Small")
-                Text("click me to go to moon! (1k points")
-                if counter >= 1000 {Button("click me to go to moon! (1k points", action: {counter -= 1000; MoonPoint += 1})
-                        .disabled(false)
-                } else if counter <= 999 {Button("click me to go to moon! (1k points", action: {counter -= 0})
-                        .disabled(true)
+            }
+            if counter == 69 {Button("funny", action: {counter -= 69; cpc += 419})
+                    .disabled(false)
+            } else if counter != 69 {Button("funny", action: {counter -= 69; cpc += 420})
+                    .disabled(true)
+                    .hidden()
+                Button {
+                    cpc += 999
+                } label: {
+                    Text("mon")
                 }
+
+            }
+            Image("moon Small")
+            //            Text("click me to go to moon! (1k points")
+            if counter >= 1000 {
+                Button("click me to go to moon! (1k points", action: {counter -= 1000; MoonPoint += 1})
+                    .disabled(false)
                 
             }
-                    if MoonPoint >= 1 {NavigationLink("moon travel"){Moon} .disabled(false)
-                    } else if MoonPoint == 0 {NavigationLink("moon trvael"){Moon}
-                            .disabled(true)
+            else if counter <= 999 {Button("click me to go to moon! (1k points)", action: {counter -= 0})
+                    .disabled(true)
+            }
             
-            
-                    }
         }
+        //                    if MoonPoint >= 1 {NavigationLink("moon travel"){Moon} .disabled(false)
+        //                    } else if MoonPoint == 0 {NavigationLink("moon trvael"){Moon}
+        //                            .disabled(true)
+        //
+        //
     }
+
+
 
 // Stretch 1: Click and make it show up
 
@@ -69,7 +77,7 @@ import SwiftUI
 // Stretch 4: Figure out how to resize images
 
 // Stretch 5: 69
-    
+
 // Stretch 6: Critical error (2)
 
 // Stretch 7: Moon
